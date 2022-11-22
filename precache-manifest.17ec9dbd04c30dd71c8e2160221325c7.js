c575afe1a7b69eccd9928d0d37393ed33f7f793b
collie <2541474139@qq.com>
2022-11-22 21:39:35 +0800

    deploy

---
+++ precache-manifest.17ec9dbd04c30dd71c8e2160221325c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39eda247b84498e86df0",
+    "url": "/bookkeeping-website/css/app.6747855b.css"
+  },
+  {
+    "revision": "f0c23bfa875ab7618b8e70882637bb89",
+    "url": "/bookkeeping-website/index.html"
+  },
+  {
+    "revision": "39eda247b84498e86df0",
+    "url": "/bookkeeping-website/js/app.c7003c52.js"
+  },
+  {
+    "revision": "6e79b7f4bc3c6323a1d7",
+    "url": "/bookkeeping-website/js/chunk-vendors.da620684.js"
+  },
+  {
+    "revision": "d1b00465103b8de3edb45d0584a5e5c4",
+    "url": "/bookkeeping-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bookkeeping-website/robots.txt"
+  }
+]);
